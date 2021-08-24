c855b78e1e02b09e9f921b7086fd3870129d7bef
Erin Nealer <77023527+enealer@users.noreply.github.com>
2021-08-24 18:58:43 -0400

    Add files via upload
    updated plot 1

--- plot1.R
+++ plot1.R
@@ -3,8 +3,10 @@ NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
 totalNEI <- NEI[, lapply(sum, na.rm = TRUE), by = year]
 
-barplot(totalNEI[, Emissions]
-        , col = "Orange"
-        , names = totalNEI[, year]
-        , xlab = "Years", ylab = "Emissions"
-        , main = "Emissions over time")
+barplot(
+  (totalNEI$Emissions),
+  names.arg=totalNEI$year,
+  xlab="Year",
+  ylab="PM2.5 Emissions (tons)",
+  main="Total PM2.5 Emissions over Time"
+)
